8899c8fdc57b0ecc29162a4887f73f682206e2dc
dennisslimmers <dennisslimmers@live.nl>
2018-06-11 20:58:18 +0200

    Fixed the wrong path to the diceware log file

--- diceware/logger.py
+++ diceware/logger.py
@@ -3,7 +3,7 @@ import sys, os
 class Logger:
     def __init__(self):
         try:
-            self.file = open("../logs/genlog.txt", 'a')
+            self.file = open("./logs/genlog.txt", 'a')
         except IOError as E:
             print(E) # No such file or directory: '../logs/genlog.txt'
             exit(0)
